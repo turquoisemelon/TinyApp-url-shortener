bc95d088130c7eae8b0c66840cff70801d800c89
Meltem <melltemkillic@gmail.com>
2017-04-12 18:10:22 -0400

    cookie-parser module added to server file

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
